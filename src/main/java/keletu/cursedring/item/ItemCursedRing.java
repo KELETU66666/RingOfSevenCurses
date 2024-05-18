f240d19cfb321881bab9bd59064ab028832d75b6
a <EYCHFSJD@gmail.com>
2024-05-18 09:03:00 +0800

    simply update

--- src/main/java/keletu/cursedring/item/ItemCursedRing.java
+++ src/main/java/keletu/cursedring/item/ItemCursedRing.java
@@ -6,6 +6,7 @@ import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Multimap;
 import keletu.cursedring.CursedRingMod;
 import static keletu.cursedring.ConfigsCR.*;
+import static keletu.cursedring.event.CREvents.hasCursed;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.resources.I18n;
@@ -89,6 +90,11 @@ public class ItemCursedRing extends Item implements IBauble {
 			list.add("");
 			list.add(I18n.format("tooltip.cursedring.holdShift"));
 		}
+	}
+
+	@Override
+	public boolean canEquip(ItemStack itemstack, EntityLivingBase player) {
+		return player instanceof EntityPlayer && !hasCursed((EntityPlayer) player);
 	}
 
 	@Override
